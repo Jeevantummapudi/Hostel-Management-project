6f05e9f17b39c0300d9e2cd5d98463f595aa45bb
HP <HP@LAPTOP-OK396O9K>
2019-09-17 11:35:45 +0530

    Successfully completed

--- src/test/java/main/StudentTest.java
+++ src/test/java/main/StudentTest.java
@@ -1,5 +1,6 @@
 package main;
 
+
 import static org.junit.Assert.*;
 import main.Hostel;
 
@@ -8,43 +9,38 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.io.IOException;	
+import java.io.IOException;
 public class StudentTest {
-	Hostel h ;
-	@SuppressWarnings("static-access")
-	@Test
-	public void studentNameTest() throws IOException {
-		boolean chk = true;
-		h	= new Hostel();
-		
-		BufferedWriter out = new BufferedWriter(new FileWriter("hostel.txt",true));
-//		assertEquals(false,h.verifyName("test rNo"));
-		
-			try{
-				out.write("tekjhgfdsst rNo");
-				out.newLine();
-	           }catch(Exception e){
-	               //print
-	               System.out.println(e);
-	               
-	           }finally{
-	               out.close();
-	           }
-		
-		
-		assertEquals(true,h.verifyName("tekjhgfdsst"));
-		
-		
-		
-		
-//		fail("Not yet implemented");
-	}
-	@SuppressWarnings("static-access")
-	@Test
-	public void studentDuplicacyTest() throws IOException{
-//		boolean chk = true;
-		assertEquals(false,h.verifyName("abcdef"));
-	}
+Hostel h ;
+private boolean chk;
+@SuppressWarnings("static-access")
+@Test
+public void studentNameTest() throws IOException {
+boolean chk = true;
+h = new Hostel();
+
+BufferedWriter out = new BufferedWriter(new FileWriter("hostel.txt",true));
+// assertEquals(false,h.verifyName("test rNo"));
+
+try{
+out.write("tekjhgfdsst rNo");
+out.newLine();
+          }catch(Exception e){
+              //print
+              System.out.println(e);
+             
+          }finally{
+              out.close();
+          }
+assertEquals(true,h.verifyName("tekjhgfdsst"));
+// fail("Not yet implemented");
+}
+@SuppressWarnings("static-access")
+@Test
+public void studentDuplicacyTest() throws IOException{
+  // boolean chk = true;
+assertEquals(true,h.verifyName("abcdef"));
+}
 
 
 }
